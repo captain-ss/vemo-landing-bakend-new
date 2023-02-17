2399700794a2943fc9ed43280bab499b5f53dcd5
Captain-ss <swadeshishu2001@gmail.com>
2023-02-18 03:35:15 +0530

    setup complete

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express=require("express");
+const express = require("express");
 const app = express();
 
 
-const start=async()=>{
-    
-    app.listen(3005)
-
-}
+const start = async () => {
+  app.listen(3005, () => {
+    console.log("server running on port 3005");
+  });
+};
+start();
